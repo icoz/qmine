24c52dd903e6180667e99376ace26b8a02466d99
icoz <icoz.vt@gmail.com>
2011-12-24 02:36:05 +0400

    new files added

---
+++ src/world/block.h
@@ -0,0 +1,11 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+class Block{
+    int x,y,z;
+    int type;
+};
+
+enum BlockType {btDirt, btStone};
+
+#endif // BLOCK_H
---
+++ src/world/chunk.h
@@ -0,0 +1,12 @@
+#ifndef CHUNK_H
+#define CHUNK_H
+
+#include <QtCore>
+
+class Chunk
+{
+public:
+    Chunk();
+};
+
+#endif // CHUNK_H
